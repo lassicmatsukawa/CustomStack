1b9e2d48c05da9d968f2700b232fe478d595028d
lassicmatsukawa <matsukawa@lassic.co.jp>
2019-11-27 20:23:32 +0900

    custom stack

---
+++ CustomStack/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    class Program
+    {
+        protected static StackExample<SampleData> stack;
+        static void Main(string[] args)
+        {
+            #region push over limit
+            stack = new StackExample<SampleData>();
+            var taro = new SampleData("Taro");
+            var hanako = new SampleData("Hanako");
+            var jiro = new SampleData("Jiro");
+            var yoko = new SampleData("Yoko");
+            var saburo = new SampleData("Saburo");
+
+            //stack.Push(taro);
+            //stack.Push(hanako);
+            //stack.Push(jiro);
+            //stack.Push(yoko);
+            //stack.Push(saburo);
+            #endregion
+
+            Console.ReadLine();
+
+            #region delete specific index
+            stack = new StackExample<SampleData>();
+
+            //stack.Push(taro);
+            //stack.Push(hanako);
+            //stack.Push(jiro);
+            //stack.Remove(2);
+            //stack.Remove(1);
+            //stack.Remove(2);
+            #endregion
+
+            Console.ReadLine();
+
+            #region get item
+            stack = new StackExample<SampleData>();
+
+            stack.Push(taro);
+            stack.Push(hanako);
+            var item = stack.Pop();
+            Console.WriteLine(item.Name);
+            item = stack.Pop();
+            Console.WriteLine(item.Name);
+            item = stack.Pop();
+            try
+            {
+                Console.WriteLine(item.Name);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("it's unsafe - {0}", ex.Message);
+            }
+            #endregion
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ CustomStack/StackExample.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomStack
+{
+    public class StackExample<T>
+    {
+        protected List<T> items;
+        protected int MAX_SIZE = 3;
+        public StackExample()
+        {
+            this.items = new List<T>();
+        }
+
+        public T Pop()
+        {
+            int itemCount = this.items.Count();
+            if (itemCount > 0)
+            {
+                T lastItem = this.items[itemCount - 1];
+                this.items.RemoveAt(itemCount - 1);
+                return lastItem;
+            }
+            // watch msdn : https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/default
+            // or just return null
+            return default(T);
+        }
+
+        public void Push(T item)
+        {
+            this.items.Add(item);
+            if (this.items.Count() > MAX_SIZE)
+            {
+                this.items.RemoveAt(0);
+            }
+            this.WatchAllitems();
+        }
+
+        public void Remove(int index)
+        {
+            if (this.items.ElementAtOrDefault(index) != null)
+            {
+                this.items.RemoveAt(index);
+            }
+            this.WatchAllitems();
+        }
+
+        private void WatchAllitems()
+        {
+            Console.WriteLine("<------------------->");
+            this.items.ForEach(e => Console.WriteLine("{0}", (e as SampleData).Name));
+            Console.WriteLine("<------------------->");
+        }
+    }
+}
